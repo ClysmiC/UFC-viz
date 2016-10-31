80bae0560a8e16ad55d512aca2e491fff142a930
ClysmiC <asmith379@gatech.edu>
2016-10-31 16:54:19 -0400

    getting an example network working, so we can modify its data into our data

--- ufc-viz.js
+++ ufc-viz.js
@@ -1,3 +1,86 @@
+var fighters = {};
+var weightClasses = ["Atomweight", "Strawweight", "Flyweight",
+					 "Bantamweight", "Featherweight", "Lightweight",
+					 "Welterweight", "Middleweight", "Light Heavyweight",
+					 "Heavyweight", "Super Heavyweight"];
+
+var MIN_FIGHT_COUNT = 10;
+
+d3.csv("fighters.csv", function(data) {
+	for(var i = 0; i < data.length; i++) {
+		var id = data[i]["fid"];
+		var name = data[i]["name"];
+		var wClass = data[i]["class"];
+		var fightList = [];
+
+		if(weightClasses.indexOf(wClass) > -1) {
+			fighters[id] = {
+				name: name,
+				wClass: wClass,
+				fightList: fightList,
+			};
+		}
+		else {
+			console.log("Unknown weight class '" + wClass + "' for fighter " + name + " (" + id + ") ... Ignoring fighter.");
+		}
+	}
+
+	// Now that fighters are loaded into memory,
+	// read all of the fight data
+	d3.csv("fights.csv", function(data) {
+		for(var i = 0; i < data.length; i++) {
+			var fight = data[i];
+			var id1 = fight["f1fid"];
+			var id2 = fight["f2fid"];
+			var name1 = fight["f1name"];
+			var name2 = fight["f2name"];
+
+			if(id1 in fighters && id2 in fighters) {
+				fighters[id1].fightList.push(
+					{
+						opponent: name2,
+						result: fight["f1result"]
+					}
+				);
+
+				fighters[id2].fightList.push(
+					{
+						opponent: name1,
+						result: fight["f2result"]
+					}
+				);
+			}
+			else {
+				if (!(id1 in fighters)) {
+					console.log("Couldn't identify fighter " + name1 + "(" + id1 + ") ... Ignoring fight.");
+				}
+				if (!(id2 in fighters)) {
+					console.log("Couldn't identify fighter " + name2 + "(" + id2 + ") ... Ignoring fight.");
+				}
+			}
+		}
+
+		// Now that fight data is loaded into memory, remove any fighters
+		// that have < MIN_FIGHT_COUNT
+		var removeList = [];
+		
+		for(var id in fighters) {
+			var fighter = fighters[id];
+
+			if(fighter.fightList.length < MIN_FIGHT_COUNT) {
+				removeList.push(id);
+			}
+		}
+
+		for(var i = 0; i < removeList.length; i++) {
+			var removeId = removeList[i];
+			delete fighters[removeId];
+		}
+
+		console.log("Removed " + removeList.length + " fighters for having < " + MIN_FIGHT_COUNT + " fights ... " + Object.keys(fighters).length + " fighters remain.");
+	});
+});
+
 var width = 960,
     height = 500
 
@@ -5,44 +88,54 @@ var svg = d3.select("body").append("svg")
     .attr("width", width)
     .attr("height", height);
 
-var force = d3.layout.force()
-    .gravity(.05)
-    .distance(100)
-    .charge(-100)
-    .size([width, height]);
+var color = d3.scaleOrdinal(d3.schemeCategory20);
 
-d3.json("graphFile.json", function(json) {
-  force
-      .nodes(json.nodes)
-      .links(json.links)
-      .start();
+var simulation = d3.forceSimulation()
+    .force("link", d3.forceLink().id(function(d) { return d.id; }))
+    .force("charge", d3.forceManyBody())
+    .force("center", d3.forceCenter(width / 2, height / 2));
 
-  var link = svg.selectAll(".link")
-      .data(json.links)
-    .enter().append("line")
-      .attr("class", "link")
-    .style("stroke-width", function(d) { return Math.sqrt(d.weight); });
+d3.json("graphFile.json", function(error, graph) {
+  var link = svg.append("g")
+      .attr("class", "links")
+      .selectAll("line")
+      .data(graph.links)
+      .enter().append("line")
+      .attr("stroke-width", function(d) { return 2; });
 
-  var node = svg.selectAll(".node")
-      .data(json.nodes)
-    .enter().append("g")
-      .attr("class", "node")
-      .call(force.drag);
+  var node = svg.append("g")
+      .attr("class", "nodes")
+      .selectAll("circle")
+      .data(graph.nodes)
+      .enter().append("g");
 
-  node.append("circle")
-      .attr("r","5");
+	node.append("circle")
+		.attr("r", 5)
+		.attr("fill", function(d) { return color(d.group); })
 
-  node.append("text")
-      .attr("dx", 12)
-      .attr("dy", ".35em")
-      .text(function(d) { return d.name });
+	node.append("text")
+		.text("hello");
 
-  force.on("tick", function() {
-    link.attr("x1", function(d) { return d.source.x; })
+  simulation
+      .nodes(graph.nodes)
+      .on("tick", ticked);
+
+  simulation.force("link")
+      .links(graph.links);
+
+  function ticked() {
+    link
+        .attr("x1", function(d) { return d.source.x; })
         .attr("y1", function(d) { return d.source.y; })
         .attr("x2", function(d) { return d.target.x; })
         .attr("y2", function(d) { return d.target.y; });
 
-    node.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
-  });
+      node.selectAll("circle")
+          .attr("cx", function(d) { return d.x; })
+          .attr("cy", function(d) { return d.y; });
+
+      node.selectAll("text")
+          .attr("x", function(d) { return d.x; })
+          .attr("y", function(d) { return d.y; });	  
+  }
 });
